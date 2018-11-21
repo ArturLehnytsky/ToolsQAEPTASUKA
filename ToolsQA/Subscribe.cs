350695f29bdd22b916c716febfa09683fffe23d1
Arturlehnytsky <arturlehnytsky@gmail.com>
2018-11-21 18:42:36 +0200

    complet reg

--- ToolsQA/Subscribe.cs
+++ ToolsQA/Subscribe.cs
@@ -1,11 +1,12 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ToolsQA
@@ -14,7 +15,7 @@ namespace ToolsQA
     {
         IWebDriver driver = new ChromeDriver();
 
-        [Test]
+    /*    [Test]
         public void TestSubscribe()
         {
 
@@ -26,6 +27,7 @@ namespace ToolsQA
             driver.FindElement(By.Id("static-subscribe-button-footer")).Click();
             driver.Close();
         }
+    */
 
         [Test]
         public void Registration()
@@ -42,15 +44,24 @@ namespace ToolsQA
             driver.FindElement(By.Id("input-address-1")).SendKeys("Kiev");
             driver.FindElement(By.Id("input-city")).SendKeys("kiev");
 
-
-
-
-
-
+                                              
            // DropDown Box Country/Region
-           // SelectElement oSelection = new SelectElement(driver.FindElement(By.Id("input-country")));
+           SelectElement cSelect = new SelectElement(driver.FindElement(By.Id("input-country")));
 
-           // oSelection.SelectByValue("67");
+           cSelect.SelectByValue("67");
+
+            Thread.Sleep(2000);
+
+            SelectElement rSelect = new SelectElement(driver.FindElement(By.Id("input-zone")));
+            rSelect.SelectByValue("1054");
+
+            // Create  Password
+            driver.FindElement(By.Id("input-password")).SendKeys("123456789");
+            driver.FindElement(By.Id("input-confirm")).SendKeys("123456789");
+
+            // Press the Button 
+            driver.FindElement(By.XPath("/html/body[@class='account-register']/div[@id='oct-bluring-box']/div[@class='container']/div[@class='col-sm-12 content-row']/div[@id='content']/form[@class='form-horizontal']/div[@class='buttons']/div[@class='pull-right']/input[@class='oct-button']")).Click();
+
 
         }
 
